a3c5874260a4b0ca57ce77016b4ab1c62241dcaf
avacaltec <avacaltec@gmail.com>
2015-02-23 16:40:01 -0500

    uuid

--- www/js/index.js
+++ www/js/index.js
@@ -12,29 +12,72 @@ var app = {
     },
 };
 
+var uuid = device.uuid;
+alert("uuid");
+
 function verAlertas() {
     document.getElementById('alertas').style.display = 'inline-block';
     document.getElementById('registro').style.display = 'none';
 }
 
-//alert("EXEC");
-function registroUsuario() {
-    alert("SUBMIT");
-    $("#registroForm").submit(function() {
 
-        var url = "http://secimo.app.gkhome.net/sys/reg/AVACAL23D02M15AV001JM/rus230215v001jm.php"; // the script where you handle the form input.
+    $('#registroUsuario').submit(function(){
+                
+                var count = 0;
+                var dni = document.forms["registroUsuario"]["dni"].value;
+                var telefono = document.forms["registroUsuario"]["telefono"].value;
+                var nombre = document.forms["registroUsuario"]["nombre"].value;
+                var email = document.forms["registroUsuario"]["email"].value;
 
-        $.ajax({
-               type: "POST",
-               url: url,
-               data: $("#registroForm").serialize(), // serializes the form's elements.
-               success: function(data)
-               {
-                   alert(data); // show response from the php script.
-               }
-             });
+                if (dni == null || dni == "" ) {
+                    count += 1;
+                }
+                if (telefono == null || telefono == "" ) {
+                    count += 1;
+                }
+                if (nombre == null || nombre == "" ) {
+                    count += 1;
+                }
+                if (email == null || email == "" ) {
+                    count += 1;
+                }
 
-        return false; // avoid to execute the actual submit of the form.
-    });
-    alert("SUBMIT2");
-}
+                if (count > 0) {
+                    alert("Por favor complete todos los campos correctamente.");
+                    return false;                    
+                }
+
+
+                var dataID = $(this).parent().attr('data-datos-id');
+                var postData = $(this).serialize();
+
+                $.ajax({
+                    type: 'POST',
+                    data: postData+'&lid='+dataID,
+                    url: 'http://secimo.app.gkhome.net/sys/reg/AVACAL23D02M15AV001JM/rus230215v001jm.php',
+                    success: function(data){
+                        console.log(data);
+
+                        $.ajax({
+                            url: 'http://mode.com.pe/s/mcvp/0.1/vmcvip1502.php',
+                            type: 'GET',
+                            dataType: 'jsonp',
+                            jsonp: 'callback',
+                            error: function(xhr, status, error) {
+                                alert("error");
+                            },
+                            success: function(jsonp) { 
+                              if (jsonp.mensaje<1) {
+                                    alert('INCORRECTO');
+                                }else{
+                                    alert('CORRECTO');
+                                    m4m42();
+                                }
+                            }
+                        });
+                    },
+                    error: function(data){ alert(data); }
+                });
+
+                return false;
+            });
