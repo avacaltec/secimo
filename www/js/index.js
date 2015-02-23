6ae2b4ccac220c73c9ae6187136f8edc84ad774b
avacaltec <avacaltec@gmail.com>
2015-02-23 09:47:44 -0500

    form

--- www/js/index.js
+++ www/js/index.js
@@ -17,8 +17,7 @@ function verAlertas() {
     document.getElementById('registro').style.display = 'none';
 }
 
-function enviarForm() {
-    document.forms["formularioRegistro"].submit(function(){
+    $('formularioRegistro').submit((function(){
                 $.ajax({
                     type: 'POST',
                     url: 'http://secimo.app.gkhome.net/sys/reg/AVACAL23D02M15AV001JM/rus230215v001jm.php',
@@ -47,4 +46,3 @@ function enviarForm() {
 
                 return false;
             });
-}
